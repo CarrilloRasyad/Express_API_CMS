17b97c46cf3a57f54c9a74f8c7462391238f5152
Carillo Rasyad Siswoyo <arcanestark@gmail.com>
2025-06-24 09:20:51 +0700

    [patch] User (service, validation, controller, api) and Contact (api)

---
+++ src/route/api.js
@@ -0,0 +1,21 @@
+import contactController from "../controller/contact-controller.js";
+import userController from "../controller/user-controller.js";
+import { authMiddleWare } from "../middleware/auth-middleware.js";
+
+
+const userRoute = new express.Router();
+userRoute.use(authMiddleWare);
+
+// User API
+userRoute.get('/api/users/current', userController.get);
+userRoute.patch('/api/users/current', userController.update);
+userRoute.delete('/api/users/logout', userController.logout);
+
+// Contact API
+userRoute.post('/api/contacts', contactController.create);
+userRoute.get('/api/contacts/:contactId', contactController.get);
+userRoute.put('/api/contacts/:contactId', contactController.update);
+userRoute.delete('/api/contacts/:contactId', contactController.remove);
+userRoute.get('/api/contacts', contactController.search);
+
+// Addresses API
